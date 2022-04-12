d366ecf467134b42b5134551dc8c7dfd729dfdcf
Rebecca Benjamin <rebecca@b-inet.com>
2022-04-12 00:20:44 -0400

    Fixed migration

---
+++ database/migrations/2022_04_12_032044_create_projects_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateProjectsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('project', function (Blueprint $table) {
+            $table->bigIncrements('project_id');
+            $table->string('project_name',50);
+            $table->string('project_description',255)->nullable();
+            $table->string('project_code',10)->unique();
+            $table->smallInteger('project_status')->default(0);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('project');
+    }
+}
