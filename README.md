bcd1deb33a9cc8ecf54e4c24ceaaf57c32b5c1e4
Rebecca Benjamin <rebecca@b-inet.com>
2022-04-12 00:29:18 -0400

    fixes migration issue

--- README.md
+++ README.md
@@ -2,7 +2,7 @@
 2. cd Xsolla/
 3. composer install
 4. Add your database config in the .env file. I have named the DB xsolla.
-5. php artisan migrate OR vendor/bin/phinx migrate(library for migration of DB)
+5. php artisan migrate
 6. Run php artisan db:seed to run faker to add data into the database table.
 7. php artisan serve. Use the ip generated to replace localhost in the .env file for APP_URL
 8. Navigate to <APP URL> to access the project table.
--- database/migrations/2022_04_12_032044_create_projects_table.php
+++ database/migrations/2022_04_12_032044_create_projects_table.php
@@ -6,11 +6,6 @@ use Illuminate\Support\Facades\Schema;
 
 class CreateProjectsTable extends Migration
 {
-    /**
-     * Run the migrations.
-     *
-     * @return void
-     */
     public function up()
     {
         Schema::create('project', function (Blueprint $table) {
@@ -23,11 +18,6 @@ class CreateProjectsTable extends Migration
         });
     }
 
-    /**
-     * Reverse the migrations.
-     *
-     * @return void
-     */
     public function down()
     {
         Schema::dropIfExists('project');
